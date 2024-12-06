8d5ea0ac5d94178d0ce3d50149866da392dbb52a
sarthakwaghpatil <119449978+sarthakwaghpatil@users.noreply.github.com>
2024-12-06 17:19:34 +0530

    Update k-fold.py

--- k-fold.py
+++ k-fold.py
@@ -5,14 +5,11 @@ from sklearn.datasets import load_iris
 
 iris = load_iris()
 
-X = iris.data  # Features: SepalLength, SepalWidth, PetalLength, PetalWidth
-y = iris.target  # Target: Species (Setosa, Versicolor, Virginica)
+X = iris.data  
+y = iris.target 
 
 clf = DecisionTreeClassifier(random_state=42)
-
 cv_scores = cross_val_score(clf, X, y, cv=5)
-
 print(f"Accuracy for each fold: {cv_scores}")
-
 mean_accuracy = cv_scores.mean()
 print(f"Mean accuracy across 5 folds: {mean_accuracy * 100:.2f}%")
